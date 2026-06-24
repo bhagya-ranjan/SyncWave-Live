89a9b64a60bfc77b66435ebf2124c55c5e341081
Bhagya <bhagyaranjan992@gmail.com>
2026-06-24 09:32:03 +0530

    logo change

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -15,8 +15,8 @@ const Topbar = () => {
   return (
     <div className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 backdrop-blur-md z-10">
         <div className="flex gap-2 items-center text-white">
-            <img src = "/spotify.png" className="size-8 " alt="Spotify logo"/>
-            Spotify
+            <img src = "/syncwave.png" className="size-10 rounded-full " alt="Spotify logo"/>
+            SyncWave
         </div>
         <div className="flex items-center gap-4">
             {isAdmin && (
